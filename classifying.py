9fa77c2e48625c38959742fe25788ff85c73238c
AhmedHammad <hammad97official@gmail.com>
2019-01-16 03:46:39 +0200

    Pre-final release

--- classifying.py
+++ classifying.py
@@ -1,50 +1,48 @@
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB, MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 
-def classify(vectors, labels, type="SVM"):
+def classify(vectors, labels, type="DT"):
     # Random Splitting With Ratio 3 : 1
     train_vectors, test_vectors, train_labels, test_labels = train_test_split(vectors, labels, test_size=0.25)
 
     # Initialize Model
     classifier = None
-    if(type=="GNB"):
-        classifier = GaussianNB()
-        classifier.fit(train_vectors, train_labels)
-    elif(type=="MNB"):
-        classifier = MultinomialNB()
+    if(type=="MNB"):
+        classifier = MultinomialNB(alpha=0.7)
         classifier.fit(train_vectors, train_labels)
     elif(type=="KNN"):
-        classifier = KNeighborsClassifier()
-        classifier = GridSearchCV(classifier, dict(n_neighbors=[3,5,7,9]), cv=3)
+        classifier = KNeighborsClassifier(n_jobs=4)
+        params = {'n_neighbors': [3,5,7,9], 'weights':['uniform', 'distance']}
+        classifier = GridSearchCV(classifier, params, cv=3, n_jobs=4)
         classifier.fit(train_vectors, train_labels)
         classifier = classifier.best_estimator_
     elif(type=="SVM"):
         classifier = SVC()
-        classifier = GridSearchCV(classifier, {'C':[0.001, 0.01, 0.1, 1, 10], 'gamma' :[0.001, 0.01, 0.1, 1]}, cv=3)
+        classifier = GridSearchCV(classifier, {'C':[0.001, 0.01, 0.1, 1, 10]}, cv=3, n_jobs=4)
         classifier.fit(train_vectors, train_labels)
         classifier = classifier.best_estimator_
     elif(type=="DT"):
-        classifier = DecisionTreeClassifier()
-        params = {'criterion':['gini','entropy'],'max_depth':[20,50,100,200,500]}
-        classifier = GridSearchCV(classifier, params, cv=3)
+        classifier = DecisionTreeClassifier(max_depth=800, min_samples_split=5)
+        params = {'criterion':['gini','entropy']}
+        classifier = GridSearchCV(classifier, params, cv=3, n_jobs=4)
         classifier.fit(train_vectors, train_labels)
         classifier = classifier.best_estimator_
     elif(type=="RF"):
-        classifier = RandomForestClassifier()
-        classifier = GridSearchCV(classifier, {'n_estimators': [n for n in range(10,100,10)]}, cv=3)
+        classifier = RandomForestClassifier(max_depth=800, min_samples_split=5)
+        params = {'n_estimators': [n for n in range(50,200,50)], 'criterion':['gini','entropy'], }
+        classifier = GridSearchCV(classifier, params, cv=3, n_jobs=4)
         classifier.fit(train_vectors, train_labels)
         classifier = classifier.best_estimator_
     elif(type=="LR"):
         classifier = LogisticRegression(multi_class='auto', solver='newton-cg',)
-        classifier = GridSearchCV(classifier, {"C":np.logspace(-3,3,7), "penalty":["l2"]}, cv=3)
+        classifier = GridSearchCV(classifier, {"C":np.logspace(-3,3,7), "penalty":["l2"]}, cv=3, n_jobs=4)
         classifier.fit(train_vectors, train_labels)
         classifier = classifier.best_estimator_
     else:
@@ -53,5 +51,8 @@ def classify(vectors, labels, type="SVM"):
 
     accuracy = accuracy_score(train_labels, classifier.predict(train_vectors))
     print("Training Accuracy:", accuracy)
-    accuracy = accuracy_score(test_labels, classifier.predict(test_vectors))
+    test_predictions = classifier.predict(test_vectors)
+    accuracy = accuracy_score(test_labels, test_predictions)
     print("Test Accuracy:", accuracy)
+    print("Confusion Matrix:", )
+    print(confusion_matrix(test_labels, test_predictions))
